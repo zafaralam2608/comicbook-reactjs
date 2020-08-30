73aad8ebcef85d68278e6155668323c4154bd64c
Md Zafar Alam <malam@axway.com>
2020-08-30 21:27:28 +0530

    web structure

---
+++ src/main/webapp/src/components/Application.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default class Application extends React.Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    componentDidUpdate() {
+    }
+
+    render() {
+        return(
+            <div>Hello World</div>
+        );
+    }
+}
